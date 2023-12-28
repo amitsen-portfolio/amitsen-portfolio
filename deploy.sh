8b9704190f31b9467178564a964ea70c77cc0aec
Amit Sen (portfolio) <admin@amitsen.info>
2023-12-28 17:40:33 +0100

    SFTP modification

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# FTP/SFTP server details
-SERVER="${SERVER}"
-USERNAME="${USERNAME}"
-PASSWORD="${PASSWORD}"
-LOCALDIR="${LOCALDIR}"
-REMOTEDIR="${REMOTEDIR}"
-
 # Commands to upload files
 # Replace 'sftp' with 'ftp' if you're using FTP
 sftp ${USERNAME}@${SERVER} <<EOF
