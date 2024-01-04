2ba49c37b408a6ca71b9cca1cfe42c800ead5431
Amit Sen <155026793+amitsen-portfolio@users.noreply.github.com>
2024-01-05 00:29:55 +0100

    v1: section divider implemented (#8)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 import Image from "next/image";
 
 export default function Home() {
   return <main className="flex flex-col items-center px-4">
     <Intro />
+    <SectionDivider />
   </main>;
 }
