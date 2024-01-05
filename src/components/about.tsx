ed3b68be541907d58e5ed7658ca9b2667350b055
Amit Sen <155026793+amitsen-portfolio@users.noreply.github.com>
2024-01-05 02:59:55 +0100

    v1: About section implemented (#9)

---
+++ src/components/about.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+// Import necessary modules and components
+import React from "react";
+import SectionHeading from "./section-heading"; // Custom component for section headings
+import { motion } from "framer-motion"; // Importing 'motion' for animation effects from Framer Motion
+
+export default function About() {
+  return (
+    // A motion section from Framer Motion for animated effects
+    <motion.section
+      className="mb-28 max-w-[50rem] text-center leading-8 sm:mb-40" // Styling for the section
+      initial={{ opacity: 0, y: 100 }} // Initial state of the animation: fully transparent and positioned 100 pixels down
+      animate={{ opacity: 1, y: 0 }} // End state of the animation: fully opaque and in the original position
+      transition={{ delay: 0.175 }} // Transition settings: animation starts after a delay of 175ms
+    >
+
+      {/* Section heading */}
+      <SectionHeading>About me</SectionHeading>
+
+      {/* Paragraph describing professional journey */}
+      <p className="mb-3">
+        During my undergrad in{" "}
+        <span className="font-medium">Computer Science & Engineering</span>, I
+        developed a deep passion for{" "}
+        <span className="font-medium">programming</span> and{" "}
+        <span className="font-medium">problem-solving</span>. My freelancing
+        journey began in the third year of undergrad, but it was after
+        completing my bachelor's that I embarked on my professional career.
+        Initially, I focused on <span className="font-medium">Java</span> for
+        Android development and then, after two years, transitioned to iOS
+        development using <span className="font-medium">Objective-C</span> upon
+        acquiring a MacBook. This led to three years of dedicated work in the
+        field before pursuing a{" "}
+        <span className="italic">master's in Computer Science</span>.
+        Post-master's, I resumed my career, this time specializing in{" "}
+        <span className="font-medium">Swift</span> for iOS and{" "}
+        <span className="font-medium">Kotlin</span> for Android, and
+        occasionally diving into{" "}
+        <span className="font-medium">React JS, NextJS, and TypeScript</span>.
+        Today, I'm a <span className="italic">full-stack developer</span> with
+        expertise in <span className="underline">Mobile Apps</span>.
+      </p>
+
+      {/* Paragraph about teaching experience and other skills */}
+      <p className="mb-3">
+        Since <span className="italic">2022</span>, I have also been imparting
+        knowledge as a <span className="font-medium">University Lecturer</span>,
+        teaching Fundamental Computer Science topics like{" "}
+        <span className="italic">
+          Programming, Data Structures, and Algorithms
+        </span>
+        . Alongside, I've completed a course in{" "}
+        <span className="underline">SEO</span> and successfully worked as a{" "}
+        <span className="italic">Freelance SEO Expert</span> for a few clients
+        in Upwork.
+      </p>
+      
+      {/* Paragraph about personal interests */}
+      <p>
+        Beyond technology, <span className="font-medium">Cricket</span> is a
+        significant part of my life; I play professionally for a club. A fervent
+        traveler, I've visited 21 countries and am excited to continue
+        exploring. My hobbies include watching movies, listening to music, and
+        I'm eager to learn skiing.
+      </p>
+    </motion.section>
+  );
+}
--- src/components/section-divider.tsx
+++ src/components/section-divider.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { motion } from "framer-motion";
+// Importing necessary modules
+import { motion } from "framer-motion"; // Importing 'motion' for animation effects from Framer Motion
 import React from "react";
 
+// SectionDivider functional component definition
 export default function SectionDivider() {
   return (
+    // A motion div from Framer Motion for animated effects
     <motion.div
+      // Styling for the divider: A vertical line, shown only on screens wider than the 'sm' breakpoint
       className="bg-slate-400 my-24 h-16 w-1 rounded-full hidden sm:block"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.125 }}
-    ></motion.div>
+      initial={{ opacity: 0, y: 100 }} // Initial state of the animation: fully transparent and positioned 100 pixels down
+      animate={{ opacity: 1, y: 0 }}   // End state of the animation: fully opaque and in the original position
+      transition={{ delay: 0.125 }}   // Transition settings: animation starts after a delay of 125ms
+    />
   );
 }
