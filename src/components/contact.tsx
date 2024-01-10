a7b6a0b802744391f02e080a4099e230159821b8
Amit Sen (portfolio) <admin@amitsen.info>
2024-01-10 22:42:43 +0100

    v1: Added environment variables to store emailjs api keys

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,28 +4,31 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail";
 import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
-
+import emailjs from "@emailjs/browser";
+import { getErrorMessage, validateEnvVariable } from "@/lib/utils";
 
 export default function Contact() {
   // This hook is used to keep track of whether the section is in view
   const { ref } = useSectionInView("Contact", 0.8);
 
   const handleSubmitAction = async (formData: FormData) => {
+    // Convert FormData to an object
+    const params = Object.fromEntries(formData.entries());
+
     try {
-      const { data } = await sendEmail(formData);
-      if (data?.error) {
-        toast.error(data?.error.message);
-        return;
-      }
-      toast.success("Message sent successfully! I'll get back to you soon.", {
-        duration: 3000,
-      });
-    } catch (err) {
-      toast.error("An unexpected error occurred. Please try again later.");
-      toast.error(`Unexpected error: ${err}`);
+      // Proceed with sending the email
+      const result = await emailjs.send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID ?? "", 
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID ?? "", 
+        params, 
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY ?? ""
+      );
+      toast.success("Email sent successfully!");
+    } catch (error) {
+      toast.error("Failed to send email: " + getErrorMessage(error));
+      console.error("EmailJS error:", error);
     }
   };
 
@@ -49,23 +52,29 @@ export default function Contact() {
         or through this form.
       </p>
 
-      <form
-        className="mt-10 flex flex-col"
-        action={handleSubmitAction}
-      >
+      <form className="mt-10 flex flex-col" action={handleSubmitAction}>
+        <input
+          name="name"
+          placeholder="Your name"
+          required
+          maxLength={50}
+          className="h-14 px-4 rounded-lg my-border-slate-300"
+        />
         <input
           type="email"
-          name="senderEmail"
+          name="email"
           placeholder="Your email"
           required
-          maxLength={100}
-          className="h-14 px-4 rounded-lg my-border-slate-300" />
+          maxLength={50}
+          className="h-14 my-3 px-4 rounded-lg my-border-slate-300"
+        />
         <textarea
           name="message"
           placeholder="Your message"
           required
           maxLength={500}
-          className="h-52 my-3 rounded-lg my-border-slate-300 p-4" />
+          className="h-52 my-3 rounded-lg my-border-slate-300 p-4"
+        />
         <SubmitBtn />
       </form>
     </motion.section>
