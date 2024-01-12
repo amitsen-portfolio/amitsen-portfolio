294fe6db77780d9f43dc66c9073673c611b16fd1
Amit Sen <155026793+amitsen-portfolio@users.noreply.github.com>
2024-01-12 20:20:20 +0100

    v1: footer implemented (#16)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
 
 // Initialize the Inter font from Google Fonts with the Latin subset
 const inter = Inter({ subsets: ["latin"] });
@@ -44,6 +45,7 @@ export default function RootLayout({
             {/* Children components - the main content of the page */}
             {children}
             <Toaster position="top-right" />
+            <Footer />
           </ActiveSectionContextProvider>
         </div>
       </body>
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className='mb-10 px-4 text-center text-slate-500'>
+        <small className='mb-2'>
+            &copy; 2024 Amit Sen. All rights reserved.
+        </small>
+    </footer>
+  )
+}
